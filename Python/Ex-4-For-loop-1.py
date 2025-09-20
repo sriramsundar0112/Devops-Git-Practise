1a61c2491fa8354248cfe7825c48e10745592b34
Sundaramoorthy <ssundaramoorthy@lnw.com>
2025-09-20 14:54:06 +0530

    Adding Ex-4-Nested For Loop

---
+++ Python/Ex-4-For-loop-1.py
@@ -0,0 +1,14 @@
+# To print for-loops
+
+# Type 1 : Iterate over strings
+
+for aplhabet in "Apple":
+    print(aplhabet)
+
+
+# Type -2 : Nested For-Loop
+
+for outer in "Apple":
+    print(outer)
+    for inner in "123456":
+        print(inner)
